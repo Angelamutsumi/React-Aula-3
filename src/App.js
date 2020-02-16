cc6016117e569a51edbf4b16f4d85997367b1f41
Angela Tsurumaki <angelamutsumi@gmail.com>
2020-02-15 21:21:01 -0300

    Form, divisão em componentes e onChange

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FormInput from './components/forminput.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      nome:"",
+      cidade:"",
+      email:"",
+      telefone:"",
+      cpf:"",
+    }
+  }
+
+  onChangeInput = (name, value) => {
+      this.setState({ [name]: value })
+  };
+
+  render() {
+
+      return (
+        <div className="App">
+          <h2>Cadastro de alunas</h2>
+            <form>
+              <FormInput name="nome" label="Nome" type="text" value={this.state.nome} onChangeInput={this.onChangeInput}/>
+              <FormInput name="cidade" label="Cidade" type="text" value={this.state.cidade} onChangeInput={this.onChangeInput}/>
+              <FormInput name="email" label="Email" type="email" placeholder="email@email.com" value={this.state.email} onChangeInput={this.onChangeInput}/>
+              <FormInput name="telefone" label="Telefone" type="number" placeholder="(xx)xxxxx-xxxx" value={this.state.telefone} onChangeInput={this.onChangeInput}/>
+              <FormInput name="cpf" label="CPF" type="number" placeholder="000.000.000-00" value={this.state.cpf} onChangeInput={this.onChangeInput}/>
+            </form>
+
+            <button>Inscrever</button>
+            
+        </div>
+      );
+    }
 }
 
 export default App;
